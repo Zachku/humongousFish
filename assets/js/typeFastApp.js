06f8344eea273ec4340c4e78b2d120d8a8b0a4ac
Zachku <j.v.siren@gmail.com>
2015-04-02 03:09:17 +0300

    Angular fix

---
+++ assets/js/typeFastApp.js
@@ -0,0 +1,150 @@
+﻿var typeFastApp = angular.module('typeFastApp', []);
+
+typeFastApp.controller('typeFastController', ['$scope', function ($scope) {
+        $scope.isInputDisabled = true;
+
+        // character combinations for exercise to be generated
+        $scope.exercises = [
+            { name : "asdfg"},
+            { name : "hjklöä"},
+            { name : "qwert"},
+            { name : "yuiop"},
+            { name : "zxcvb"},
+            { name : "nm,.-" },
+            { name : "12345"},
+            { name : "67890" },
+            { name : "1234567890" },
+            { name : "qwertyuipåasdfghjklöääzxcvbnm" },
+            { name : "qwertyuipåasdfghjklöääzxcvbnm1234567890" },
+        ];
+        
+        // possible lengths for test
+        $scope.wordCounts = [
+            { count: 20 },
+            { count: 30 },
+            { count: 40 },
+            { count: 50 },
+            { count: 60 },
+            { count: 70 },
+            { count: 80 },
+            { count: 90 },
+            { count: 100 },
+        ];
+        $scope.wordCount = $scope.wordCounts[1];
+
+        $scope.currentExercise = "";
+        
+        
+
+        var errors = 0; 
+        var audio = new Audio('./audio/punch.mp3');
+        var started = false;
+        
+
+        var startTime;
+        var endTime;
+        
+        $scope.end;
+        $scope.start;
+        
+        /*
+         * Test has started and user has pressed the button 
+         */
+        $scope.check = function () {
+            var input = $scope.textInput;
+            var lengthOfInput = input.length;
+            var lastCharOfInput = input.charAt(lengthOfInput - 1);
+            
+            var exercise = $scope.currentExercise;
+            var firstCharOfExercise = exercise.charAt(0);
+            
+            if (started == false) {
+                started = true;
+                startTime = new Date();
+                passStartTime();
+            }    
+            if (lastCharOfInput == firstCharOfExercise || (lastCharOfInput == " " && firstCharOfExercise == " ")) { 
+                $scope.currentExercise = exercise.substring(1, exercise.length);
+                $scope.exerciseText = $scope.currentExercise;
+                if (exercise.length <= 1) {
+                    endTime = new Date();
+
+                    $scope.currentExercise = "";
+                    var timeItTook = (endTime.getTime() - startTime.getTime()) / 1000;
+                    var wmp = $scope.wordCount.count / (timeItTook / 60);
+                    
+                    passEndTime();
+                    alert("Finish! You had " + errors + " typos. It took: " + timeItTook + " seconds and your WPM is " + wmp);
+                }
+            } else {
+                $scope.textInput = input.substring(0, input.length-1);
+                errors++;
+                audio.pause();
+                audio = new Audio('./audio/punch.mp3');
+                audio.play();
+            }
+        };
+        
+        /*
+         * Autogenerate function to create texts to write
+         */
+        $scope.generateText = function () {
+            var possibleChars = $scope.exercise.name;
+            var generatedEx = "";
+
+            var minLengthOfWord = 3;
+            var maxLengthOfWord = 7;
+            var lengthOfWord = 5;
+            
+            for (var i = 0; i < $scope.wordCount.count; i++) {
+                var word = "";
+                lengthOfWord = minLengthOfWord + Math.floor(Math.random() * (maxLengthOfWord - minLengthOfWord));
+                for (var j = 0; j < lengthOfWord; j++) {
+                    generatedEx += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+                }
+                if (i < $scope.wordCount.count - 1) {
+                    generatedEx += " ";
+                }                ;
+            }
+            $scope.currentExercise = generatedEx;
+            $scope.exerciseText = $scope.currentExercise;
+            $scope.reset();
+        };
+        
+        $scope.submitOwnText = function () {
+            $scope.currentExercise = $scope.ownExerciseInput;
+            $scope.exerciseText = $scope.ownExerciseInput;
+            $scope.reset();
+        }
+        
+        /*
+         * Reset the application
+         */
+        $scope.reset = function () {
+            $scope.start = "0";
+            $scope.end = "0";
+            typos = 0;
+            $scope.textInput = "";
+            started = false;
+            $scope.isInputDisabled = false;
+            $('#textInput').focus();
+        }
+        
+        
+        /*
+         *  Pass start and end time to view 
+         */
+        passStartTime = function () {
+            var h = startTime.getHours();
+            var min = startTime.getMinutes() > 10 ? startTime.getMinutes() : "0" + startTime.getMinutes();
+            var sec = startTime.getSeconds() > 10 ? startTime.getSeconds() : "0"+ startTime.getSeconds();
+            $scope.start = h + ":" + min + ":" + sec;
+        }
+        passEndTime = function () {
+            var h = endTime.getHours();
+            var min = endTime.getMinutes() > 10 ? endTime.getMinutes() : "0" + endTime.getMinutes();
+            var sec = endTime.getSeconds() > 10 ? endTime.getSeconds() : "0" + endTime.getSeconds();
+            $scope.end = h + ":" + min + ":" + sec;
+        }
+
+}]);
